c67909659ce5536ceb447c9f8c195df8b9a1f946
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2022-08-21 23:51:51 +0530

    Add new data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000 
-const data = require('./data.json');
+const data = require('./newData.json');
 
 app.use(require('cors')({ origin: true }));
 
